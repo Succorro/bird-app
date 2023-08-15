cb03de95158964c0ddec46b7b5d616389c215861
Steven Gutierrez <stevogbmv@gmail.com>
2023-08-15 13:26:15 -0700

    Add show commit

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,11 @@ class BirdsController < ApplicationController
         birds = Bird.all 
         render json: birds, status: :ok
     end
+
+    def show 
+        bird = Bird.find(params[:id])
+        rendre json: bird 
+    rescue ActiveRecord::RecordNotFound 
+        rendr json: "Bird not found", status: :not_found
+    end 
 end
